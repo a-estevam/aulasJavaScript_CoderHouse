74e0eca259700182e960d28de7561d17df386772
a-estevam <estevam.adesign@gmail.com>
2024-10-09 21:57:20 -0300

    entrega aula 2

--- aula2.js
+++ aula2.js
@@ -1,11 +1,12 @@
 
 var nome = prompt("Digite seu nome");
+var sobreNome = prompt("Digite seu Sobre nome");
 var anoAtual = 2024;
 
 var anoNascimento = parseInt(prompt("digite seu ano de nascimento"));
 var idade = anoAtual - anoNascimento;
 
-alert('Sua idade é '+ idade + ' anos');
+alert(nome + " " + sobreNome +' a sua idade é '+ idade + ' anos');
 
 
 console.log("Seja muito bem-vindo(a) " , nome);
