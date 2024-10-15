561e8f43f5e255d61fad85e73544a83600607b09
a-estevam <estevam.adesign@gmail.com>
2024-10-14 22:35:11 -0300

    aula 3 js

---
+++ aula3.js
@@ -0,0 +1,80 @@
+// let numero = parseInt(prompt('digite um numero de 1 a 10'));
+
+
+// if (numero <= 5){
+//     alert('é menor ou igual a 5')
+// } else{
+//     alert("é maior que 5")
+// }
+
+// let nomeUsuario = prompt   ("digite seu nome")
+
+// if(nomeUsuario== ""){
+//     alert("nome vazio")  
+// }else{
+//     alert("seu nome é: "+ nomeUsuario)
+// }
+
+
+// var combustivel = prompt('digite o combustivel');
+
+// if(combustivel =="gnv"){
+//     alert("o carro é movido a gnv")
+// }else if (combustivel == "gasolina"){
+//     alert ("o carro é movido a gasolina")
+// } else if (combustivel == 'alcool'){
+//     alert(" o carro é movido a alcool")
+// } else{
+//     alert('O carro não vai funcionar')
+// }
+
+
+// let numero = parseInt(prompt("digite um número de 1 a 5"));
+
+// if(numero == 1){
+//     alert(' você digitou o número um (1)')
+// } else if (numero == 2){
+//     alert(' você digitou o número dois (2)')
+// }else if (numero == 3){
+//     alert(' você digitou o número três (3)')
+// }else if(numero == 4){
+//     alert(' você digitou o número quatro (4)')
+// }else if(numero == 5){
+//     alert(' você digitou o número cinco (5)')
+// }else{
+//     alert('não entendi, digite um número de 1 a 5')
+// }
+
+
+// var valor = ""
+// if (valor){
+//     alert ("valor verdadeiro")
+// }else {
+//     alert ("valor falso")
+// }
+
+// if ('true' == true)
+
+
+// var numero = parseInt(Prompt("digite um numero entre 1 e 100"));
+
+
+// if (numero <= 10){
+//     alert(numero + 'está entre 1 e 10')
+// }else if(numero <=20){
+//     alert(numero + 'está entre 11 e 20')
+// }
+
+
+
+var letra = prompt('digite uma letra');
+
+if (letra == ""){
+    alert('está vazio, digite uma letra')
+
+} else if ((letra =="a")||(letra =="e") ||(letra =="i")||(letra =="o")||(letra =="u")||(letra =="A")||(letra =="E") ||(letra =="I")||(letra =="O")||(letra =="U")){
+    alert("a letra '" + letra + "' é uma vogal")
+
+}else{
+    alert ("a letra '" + letra + "' é uma consoante")
+}
