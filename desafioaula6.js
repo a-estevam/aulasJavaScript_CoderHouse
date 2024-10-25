363d401a00f18958b97ba64754b5328171108d59
a-estevam <estevam.adesign@gmail.com>
2024-10-24 22:55:40 -0300

    desavfio aula 06 - INCOMPLETO 2

--- desafioaula6.js
+++ desafioaula6.js
@@ -1,32 +1,42 @@
+
+
 function emprestimo() {
-  //valor à emprestar
-  // numero de parcelas
-  // juros
+  let taxaDeJuros = 1.49 / 100
 
-  let valorEmprestado = parseFloat(prompt("Qual valor você quer emprestado?"));
-  let parcelas = parseInt(prompt("em quantas parcelas você deseja pagar?"));
-  let valorParcela = valorEmprestado / parcelas;
-  let juros = 0.5;
 
-  let totalJuros = valorEmprestado * juros * parcelas;
 
-  if (parcelas < 1 || parcelas > 12) {
-    alert("Somente parcelas de 1 à 12");
-  } else {
-    console.log(
-      `Empréstimo de R$${valorEmprestado} parcelado em ${parcelas}x:`
-    );
+  let valorSolicitado = parseFloat(prompt("Valor do empréstimo"));
 
-    do {
-      for (let i = 1; i <= parcelas; i++) {
-        let resultado = +valorParcela + juros * i;
-        console.log(`parcela ${i} R$${resultado.toFixed(2)}`);
-        valorEmprestado = parseFloat(
-          prompt("Qual valor você quer emprestado?")
-        );
-      }
-    } while (valorEmprestado == "");
+  
+  if(isNaN(valorSolicitado) || valorSolicitado <= 0){
+    alert('digite um valor válido')
+    return 
   }
+
+  let quantidadeDeParcelas = parseInt(prompt("Quantidade de parcelas"));
+  if(isNaN(quantidadeDeParcelas) || quantidadeDeParcelas < 1 || quantidadeDeParcelas > 12){
+    alert("digite de 1 à 12x")
+    return
+  }
+
+  let valorComJuros = valorSolicitado * (1 + (taxaDeJuros * quantidadeDeParcelas))
+  let valorPorParcelaComJuros = valorComJuros / quantidadeDeParcelas
+
+    console.log(`R$${valorSolicitado} parcelado em ${quantidadeDeParcelas}x`)
+
+
+    for(let i = 1; i <= quantidadeDeParcelas; i++){
+  
+      console.log(`Parcela ${i} - R$${valorPorParcelaComJuros.toFixed(2)}`)
+    
+   }
+
+  //  while(valorSolicitado != "sair")
+  //   valorSolicitado = parseFloat(prompt("Valor do empréstimo"));
+
+
+
 }
 
 emprestimo();
+
