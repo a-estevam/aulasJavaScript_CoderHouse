876c751d21b32b76e03a93884a7bf5280603c1ac
Antonio Estevam <113124669+a-estevam@users.noreply.github.com>
2024-10-25 13:31:37 -0300

    Update desafioaula6.js
    Indentação

--- desafioaula6.js
+++ desafioaula6.js
@@ -1,43 +1,33 @@
-
-
 function emprestimo() {
-  let taxaDeJuros = 1.49 / 100
-
-  let nome = prompt("qual é o seu nome")
-  let desejaContinuar = prompt("deseja simular um empréstimo? sim/não")
-
-  while(desejaContinuar.toLowerCase() != "não"){
-    let valorSolicitado = parseFloat(prompt("Valor do empréstimo"));
-
+    let taxaDeJuros = 1.49 / 100
+    let nome = prompt("qual é o seu nome")
+    let desejaContinuar = prompt("deseja simular um empréstimo? sim/não")
   
-  if(isNaN(valorSolicitado) || valorSolicitado <= 0){
-    alert('digite um valor válido')
-    return 
+    while(desejaContinuar.toLowerCase() != "não"){
+        let valorSolicitado = parseFloat(prompt("Valor do empréstimo"));
+        if(isNaN(valorSolicitado) || valorSolicitado <= 0){
+            alert('digite um valor válido')
+            return 
+        }
     
-  }
-
-  let quantidadeDeParcelas = parseInt(prompt("Quantidade de parcelas"));
-  if(isNaN(quantidadeDeParcelas) || quantidadeDeParcelas < 1 || quantidadeDeParcelas > 12){
-    alert("digite de 1 à 12x")
-    return
-  
-  }
-
-  let valorComJuros = valorSolicitado * (1 + (taxaDeJuros * quantidadeDeParcelas))
-  let valorPorParcelaComJuros = valorComJuros / quantidadeDeParcelas
-
-    console.log(`R$${valorSolicitado.toLocaleString('pt-br', {minimumFractionDigits: 2})} parcelado em ${quantidadeDeParcelas}x`)
-
-
-    for(let i = 1; i <= quantidadeDeParcelas; i++){
-  
-      console.log(`Parcela ${i} - R$${valorPorParcelaComJuros.toLocaleString('pt-br', {minimumFractionDigits: 2})}`)
+        let quantidadeDeParcelas = parseInt(prompt("Quantidade de parcelas"));
+        if(isNaN(quantidadeDeParcelas) || quantidadeDeParcelas < 1 || quantidadeDeParcelas > 12){
+            alert("digite de 1 à 12x")
+            return
+        }
     
-   }
-   console.log(`${nome}, você irá pagar: R$${valorComJuros.toLocaleString('pt-br', {minimumFractionDigits: 2})}!`)
-   desejaContinuar = prompt("deseja fazer uma nova simulação? sim/não")
+        let valorComJuros = valorSolicitado * (1 + (taxaDeJuros * quantidadeDeParcelas))
+        let valorPorParcelaComJuros = valorComJuros / quantidadeDeParcelas
+    
+        console.log(`R$${valorSolicitado} parcelado em ${quantidadeDeParcelas}x`)
+    
+    
+        for(let i = 1; i <= quantidadeDeParcelas; i++){
+            console.log(`Parcela ${i} - R$${valorPorParcelaComJuros.toFixed(2)}`)
+        }
+        console.log(`${nome}, você irá pagar: R$${valorComJuros}!`)
+        desejaContinuar = prompt("deseja fazer uma nova simulação? sim/não")
+    }
   }
-
-}
-
-emprestimo();
+  
+  emprestimo();
