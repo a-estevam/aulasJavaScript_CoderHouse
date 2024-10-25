85dabbd6f29a6d049e1ad88ff4888834061c2ea1
Antonio Estevam <113124669+a-estevam@users.noreply.github.com>
2024-10-25 11:49:09 -0300

    Update desafioaula6.js

--- desafioaula6.js
+++ desafioaula6.js
@@ -2,41 +2,38 @@
 
 function emprestimo() {
   let taxaDeJuros = 1.49 / 100
+  let nome = prompt("qual é o seu nome")
+  let desejaContinuar = prompt("deseja simular um empréstimo? sim/não")
 
-
-
-  let valorSolicitado = parseFloat(prompt("Valor do empréstimo"));
-
+  while(desejaContinuar != "não"){
+      let valorSolicitado = parseFloat(prompt("Valor do empréstimo"));
+    if(isNaN(valorSolicitado) || valorSolicitado <= 0){
+      alert('digite um valor válido')
+      return 
+      
+    }
   
-  if(isNaN(valorSolicitado) || valorSolicitado <= 0){
-    alert('digite um valor válido')
-    return 
-  }
-
-  let quantidadeDeParcelas = parseInt(prompt("Quantidade de parcelas"));
-  if(isNaN(quantidadeDeParcelas) || quantidadeDeParcelas < 1 || quantidadeDeParcelas > 12){
-    alert("digite de 1 à 12x")
-    return
-  }
-
-  let valorComJuros = valorSolicitado * (1 + (taxaDeJuros * quantidadeDeParcelas))
-  let valorPorParcelaComJuros = valorComJuros / quantidadeDeParcelas
-
-    console.log(`R$${valorSolicitado} parcelado em ${quantidadeDeParcelas}x`)
-
-
-    for(let i = 1; i <= quantidadeDeParcelas; i++){
-  
-      console.log(`Parcela ${i} - R$${valorPorParcelaComJuros.toFixed(2)}`)
+    let quantidadeDeParcelas = parseInt(prompt("Quantidade de parcelas"));
+    if(isNaN(quantidadeDeParcelas) || quantidadeDeParcelas < 1 || quantidadeDeParcelas > 12){
+      alert("digite de 1 à 12x")
+      return
     
-   }
-
-  //  while(valorSolicitado != "sair")
-  //   valorSolicitado = parseFloat(prompt("Valor do empréstimo"));
-
-
-
+    }
+  
+    let valorComJuros = valorSolicitado * (1 + (taxaDeJuros * quantidadeDeParcelas))
+    let valorPorParcelaComJuros = valorComJuros / quantidadeDeParcelas
+  
+      console.log(`R$${valorSolicitado} parcelado em ${quantidadeDeParcelas}x`)
+  
+  
+      for(let i = 1; i <= quantidadeDeParcelas; i++){
+    
+        console.log(`Parcela ${i} - R$${valorPorParcelaComJuros.toFixed(2)}`)
+      
+     }
+     console.log(`${nome}, você irá pagar: R$${valorComJuros}!`)
+     desejaContinuar = prompt("deseja fazer uma nova simulação? sim/não")
+    }
 }
 
 emprestimo();
-
